c6870e6e141e58e248a61df361c41d2dc4b7f46a
AjayPieris <ajayperies10@gmail.com>
2025-09-21 16:46:20 +0530

    Header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import search_icon from '../../assets/search_icon.svg'
-import bell_icon from '../../assets/bell_icon.svg'
-import profile_icon from '../../assets/profile_img.png'
-import caret_icon from '../../assets/caret_icon.svg'
-
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_icon from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
 
 function Navbar() {
   return (
     <>
-      <div className='navbar'>
-        <div className='navbar-left'>
+      <div className="navbar">
+        <div className="navbar-left">
           <img src={logo} alt="Netflix Logo" />
           <ul>
             <li>Home</li>
@@ -22,17 +21,21 @@ function Navbar() {
             <li>Browse by Languages</li>
           </ul>
         </div>
-        <div className='navbar-right'>
-          <img className='icons' src={search_icon} alt="Search" />
+        <div className="navbar-right">
+          <img className="icons" src={search_icon} alt="Search" />
           <p>Childrean</p>
-          <img className='icons' src={bell_icon} alt="Avatar" />
-          <img className='icons' src={profile_icon} alt="Profile" />
-          <img className='icons' src={caret_icon} alt="Profile" />
+          <img className="icons" src={bell_icon} alt="Avatar" />
+          <div className="navbar-profile">
+            <img className="profile" src={profile_icon} alt="Profile" />
+            <img className="icons" src={caret_icon} alt="Profile" />
+            <div className="dropdown">
+              <p>Sign Out of Netflix</p>
+            </div>
+          </div>
         </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../../public/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 function Home() {
   return (
-    <div>
+    <div className='home'>
       <Navbar />
-      
+      <div className='hero'>
+        <img src={hero_banner} alt='' className='banner-img'/>
+        <div className='hero-caption'>
+          <img src={hero_title} alt='' className='caption-img'/>
+          <p>
+            Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his daring plan.
+          </p>
+          <div className='hero-btns'>
+            <button className='btn'><img src={play_icon} alt=''/>Play</button>
+             <button className='btn dark-btn'><img src={info_icon} alt=''/>More Info</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
